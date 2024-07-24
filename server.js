d4d818cc28eca93212126a0c389f35a9c9bac0c4
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-24 15:05:15 +0100

    auto-commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 
-const { TOKEN, SERVER_URL } = process.env;
+const { TOKEN, SERVER_URL, CHAT_ID } = process.env;
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
 const URI = `webhook/${TOKEN}`;
 const WEBHOOK_URL = `${SERVER_URL}/${URI}`;
