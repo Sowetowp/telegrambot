7260b57baad4e4f98d0854c12677f31102cb1d6b
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-24 11:16:42 +0100

    auto-commit

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const axios = require("axios")
 const {TOKEN, SERVER_URL} = process.env
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`
 const URI = `webhook/${TOKEN}`
-const WEBHOOK_URL = 
+const WEBHOOK_URL = SERVER_URL+URI
