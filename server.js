11d1b8cecb203e4ec9f08e4951b645908f6a75af
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-24 15:01:09 +0100

    auto-commit

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.post(`/webhook/${TOKEN}`, async (req, res) => {
     console.log(req.body);
 
     const chatId = req.body.message.chat.id
+    console.log(chatId)
     const text = req.body.message.text
     await axios.post(`${TELEGRAM_API}/sendMessage`, {
         chat_id: chatId,
